5e5ea53fa91b3c7855e7115bafcd7cb1c69adce3
jihun <79701544+yangjihun@users.noreply.github.com>
2024-03-01 17:00:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 680 ms, Memory: 65296 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+li = [[] for _ in range(N+1)]
+visited = [False]*(N+1)
+count = 0
+def BFS(v):
+  visited[v] = True
+  for i in li[v]:
+    if not visited[i]:
+      BFS(i)
+
+for i in range(M):
+  a,b = map(int,input().split())
+  li[a].append(b)
+  li[b].append(a)
+
+for i in range(1,N+1):
+  if not visited[i]:
+    count+=1
+    BFS(i)
+print(count)
