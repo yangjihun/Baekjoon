6a92820582da2c13bb36b1af8c43a423c503b142
jihun <79701544+yangjihun@users.noreply.github.com>
2025-08-17 15:50:17 +0900

    [Silver II] Title: N과 M (12), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,15 @@
+N,M = map(int,input().split())
+num_list = sorted(list(map(int,input().split())))
+li = []
+def dfs(num):
+    check = 0
+    if len(li)==M:
+        print(' '.join(map(str,li)))
+        return
+    for i in range(num,N):
+        if check!=num_list[i]:
+            check = num_list[i]
+            li.append(num_list[i])
+            dfs(i)
+            li.pop()
+dfs(0)
